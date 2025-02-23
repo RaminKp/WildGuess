e59f78625050a00a2d20d77b3e7e1991deb60633
Ramin Kupaei <r.kupaei@gmail.com>
2025-02-22 19:30:29 -0800

    Update urls.py

--- Workspace/mainpage/urls.py
+++ Workspace/mainpage/urls.py
@@ -3,5 +3,8 @@ from . import views #represents the current directory
 
 urlpatterns = [
     path("", views.helloWorld, name='helloWorld'),
-    path("penguin/", views.penguin, name='test')
+    path("penguin/", views.penguin, name='penguin'),
+    path("giraffe/", views.giraffe, name='giraffe'),
+    path("wolf/", views.wolf, name='wolf'),
+    path("ant/", views.ant, name='ant'),
 ]
