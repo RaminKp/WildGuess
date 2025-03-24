e7d7930608a1e2bc76fb9e4294f68f851866cadc
Ramin Kupaei <r.kupaei@gmail.com>
2025-03-24 16:54:21 -0700

    Update models.py
    Added button and time log

--- Workspace/mainpage/models.py
+++ Workspace/mainpage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.utils.timezone import now
 # Create your models here.
+
+
+class ButtonPressLog(models.Model):
+  button_name = models.CharField(max_length=50)  # E.g., "Next", "Submit"
+  timestamp = models.DateTimeField(default=now)  # Auto record the time
+
+  def __str__(self):
+    return f"{self.button_name} pressed at {self.timestamp}"
